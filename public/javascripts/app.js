53e21ee6a1ccd48a516186aac877f0abe1bb3b21
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-08-09 19:38:26 +0200

    Examples and linking

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,6 +9,25 @@ $(function() {
     })
   }
 
+  var examples = {
+    heroAndFriends: {
+      query: "query HeroAndFriends {\n  hero {\n    name\n    friends {\n      name\n    }\n  }\n}"
+    },
+
+    fullIntrospection: {
+      query: 'query IntrospectionQuery {\n  __schema {\n    queryType { name }\n    mutationType { name }\n    types {\n      ...FullType\n    }\n    directives {\n      name\n      args {\n        name\n        type { ...TypeRef }\n        defaultValue\n      }\n      onOperation\n      onFragment\n      onField\n    }\n  }\n}\n\nfragment FullType on __Type {\n  kind\n  name\n  fields {\n    name\n    args {\n      name\n      type { ...TypeRef }\n      defaultValue\n    }\n    type {\n      ...TypeRef\n    }\n    isDeprecated\n    deprecationReason\n  }\n  inputFields {\n    name\n    type { ...TypeRef }\n    defaultValue\n  }\n  interfaces {\n    ...TypeRef\n  }\n  enumValues {\n    name\n    isDeprecated\n    deprecationReason\n  }\n  possibleTypes {\n    ...TypeRef\n  }\n}\n\nfragment TypeRef on __Type {\n  kind\n  name\n  ofType {\n    kind\n    name\n    ofType {\n      kind\n      name\n      ofType {\n        kind\n        name\n      }\n    }\n  }\n}'
+    },
+
+    humanWithVariable: {
+      query: "query VariableExample($humanId: String!){\n  human(id: $humanId) {\n    name,\n    homePlanet,\n    friends {\n      name\n    }\n  }\n}",
+      args: '{\n  "humanId": "1000"\n}'
+    },
+
+    fragmentsExample: {
+      query: 'query FragmentExample {\n  human(id: "1003") {\n  ...Common\n    homePlanet\n  }\n\n  droid(id: "2001") {\n  ...Common\n    primaryFunction\n  }\n}\n\nfragment Common on Character {\n  name\n  appearsIn\n}'
+    }
+  }
+  
   var editorTheme = "ace/theme/github"
   var initialQuery = "query HeroAndFriends {\n  hero {\n    name\n    friends {\n      name\n    }\n  }\n}"
 
@@ -36,6 +55,24 @@ $(function() {
   var responseEditor = createEditor("responseEditor", "json", true)
 
   queryEditor.setValue(initialQuery, -1)
+
+  var updateFromUrl = function (url) {
+    var params = new URI(url ? url : document.location).search(true)
+
+    if (params.query)
+      queryEditor.setValue(params.query, -1)
+
+    if (params.args) {
+      variablesEditor.setValue(params.args, -1)
+      $("#variablesSection").collapse('show')
+    } else {
+      variablesEditor.setValue("", -1)
+      $("#variablesSection").collapse('hide')
+    }
+
+    if (params.operation)
+      $("#operation").val(params.operation)
+  }
 
   var execute = function () {
     var queryParams = {
@@ -98,4 +135,22 @@ $(function() {
   $(document).bind("keydown", "v", toggleVariables)
   $(document).bind("keydown", "c", toggleConfig)
   $(".ace_editor textarea").bind("keydown", "ctrl+return", execute)
+
+
+  updateFromUrl()
+
+  History.Adapter.bind(window,'statechange',function () {
+    updateFromUrl(History.getState().url)
+  });
+
+  var setState = function (query) {
+    History.pushState({state:1}, document.title, new URI().search(query).search())
+  }
+
+  $(".exampleLink").click(function (e) {
+    e.preventDefault()
+
+    if (examples[e.target.id])
+      setState(examples[e.target.id])
+  })
 })
